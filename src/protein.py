67f4141659774e28e6fd455ac11d74920e200307
Ninaad Kalla <ninaadkalla@gmail.com>
2024-04-29 17:30:09 -0400

    created class to read FASTA files for proteins

---
+++ src/protein.py
@@ -0,0 +1,27 @@
+class ProteinManager:
+
+    def __init__(self, fasta_path=None):
+        self.fasta_content = ""
+        if fasta_path:
+            self.load_fasta(fasta_path)
+
+    def load_fasta(self, fasta_path):
+        try:
+            with open(fasta_path, "r") as fasta_file:
+                self.fasta_content = fasta_file.read()
+        except FileNotFoundError:
+            print(f"Error: The file {fasta_path} does not exist.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def get_fasta(self):
+        return self.fasta_content
+
+    def get_sequence(self):
+        return self.fasta_content.split("\n")[1]
+
+
+if __name__ == "__main__":
+
+    protein_1 = ProteinManager("data/protein_data/1ao6.fasta")
+    print(protein_1.get_sequence())
