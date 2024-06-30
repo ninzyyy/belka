66ee91e178f40e40440f833fc713b5333de4c094
Ninaad Kalla <ninaadkalla@gmail.com>
2024-06-30 02:42:45 -0400

    added avalon fingerprint

--- src/feature_engineer.py
+++ src/feature_engineer.py
@@ -2,6 +2,8 @@ import numpy as np
 import polars as pl
 from rdkit import Chem
 from rdkit.Chem import rdFingerprintGenerator, MACCSkeys
+from rdkit.Avalon.pyAvalonTools import GetAvalonFP
+import datamol as dm
 from logger import Logger
 
 
@@ -34,7 +36,7 @@ class FeatureEngineer:
         molecule = Chem.MolFromSmiles(smiles)
         if molecule is None:
             return np.zeros(167, dtype=np.uint8)
-        return np.array(MACCSkeys.GenMACCSKeys(molecule))
+        return np.array(MACCSkeys.GenMACCSKeys(molecule), dtype=np.uint8)
 
     def generate_maccs_arr(self, df: pl.DataFrame, smiles_col: str):
         self.logger.timed_print(f"Generating MACCS features...")
@@ -42,6 +44,22 @@ class FeatureEngineer:
         maccs_arr = np.vectorize(self.generate_maccs, signature="()->(n)")
         maccs_arr = maccs_arr(smiles_arr)
         self.logger.timed_print(
-            f"MACCS keys: {maccs_arr.shape[1]} features for {smiles_arr.shape[0]} molecules."
+            f"MACCS keys: {maccs_arr.shape[1]} features for {maccs_arr.shape[0]} molecules."
         )
         return maccs_arr
+
+    def generate_avalon(self, smiles):
+        molecule = Chem.MolFromSmiles(smiles)
+        if molecule is None:
+            return np.zeros(512, dtype=np.float32)
+        return np.array(GetAvalonFP(molecule, nBits=512))
+
+    def generate_avalon_arr(self, df: pl.DataFrame, smiles_col: str):
+        self.logger.timed_print(f"Generating Avalon features...")
+        smiles_arr = df[smiles_col].to_numpy()
+        aval_arr = np.vectorize(self.generate_avalon, signature="()->(n)")
+        aval_arr = aval_arr(smiles_arr)
+        self.logger.timed_print(
+            f"Avalon fingerprint: {aval_arr.shape[1]} features for {aval_arr.shape[0]} molecules."
+        )
+        return aval_arr
