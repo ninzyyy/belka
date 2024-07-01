4cd1555383478aac24fde12132e7ca0af9a55a3c
Ninaad Kalla <ninaadkalla@gmail.com>
2024-07-01 01:22:56 -0400

    grouped fp generation and concatenate, added batching for memory purposes

--- src/feature_engineer.py
+++ src/feature_engineer.py
@@ -1,9 +1,10 @@
 import numpy as np
 import polars as pl
+
 from rdkit import Chem
 from rdkit.Chem import rdFingerprintGenerator, MACCSkeys
 from rdkit.Avalon.pyAvalonTools import GetAvalonFP
-import datamol as dm
+
 from logger import Logger
 
 
@@ -11,55 +12,56 @@ class FeatureEngineer:
 
     def __init__(self):
         self.logger = Logger()
-        self.fp_gen = rdFingerprintGenerator.GetMorganGenerator(
-            radius=2,
-            fpSize=2048,
-        )
+        self.ecfp_gen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
 
-    def generate_ecfp(self, smiles):
+    def smiles_to_fp(self, smiles):
+
         molecule = Chem.MolFromSmiles(smiles)
         if molecule is None:
-            return np.zeros(self.fp_gen.GetLength(), dtype=np.uint8)
-        return self.fp_gen.GetFingerprintAsNumPy(molecule)
+            return np.zeros(self.ecfp_gen.GetLength() + 167 + 512, dtype=np.float32)
 
-    def generate_ecfp_arr(self, df: pl.DataFrame, smiles_col: str):
-        self.logger.timed_print(f"Generating ECFP features...")
-        smiles_arr = df[smiles_col].to_numpy()
-        ecfp_arr = np.vectorize(self.generate_ecfp, signature="()->(n)")
-        ecfp_arr = ecfp_arr(smiles_arr)
+        ecfp = self.ecfp_gen.GetFingerprintAsNumPy(molecule)
+        maccs = np.array(MACCSkeys.GenMACCSKeys(molecule), dtype=np.float32)
+        avalon = np.array(GetAvalonFP(molecule, nBits=512), dtype=np.float32)
+
+        return np.concatenate([ecfp, maccs, avalon])
+
+    def generate_fp_batch(
+        self,
+        df: pl.DataFrame,
+        smiles_col: str,
+        batch_size: int,
+        verbose: bool,
+    ):
+
+        self.logger.timed_print(f"Generating fingerprint features...")
+        total, processed = len(df), 0
+
+        for batch in df.iter_slices(batch_size):
+            smiles_arr = batch[smiles_col].to_numpy()
+            features = np.array([self.smiles_to_fp(smiles) for smiles in smiles_arr])
+
+            processed += len(smiles_arr)
+            if verbose:
+                self.logger.timed_print(f"Processed {processed}/{total} molecules")
+
+            yield features
+
+    def generate_fp_arr(
+        self,
+        df: pl.DataFrame,
+        smiles_col: str = "molecule_smiles",
+        batch_size: int = 10000,
+        verbose: bool = True,
+    ):
+        all_features = []
+        for batch_features in self.generate_fp_batch(
+            df=df, smiles_col=smiles_col, batch_size=batch_size, verbose=verbose
+        ):
+            all_features.append(batch_features)
+
+        fp_arr = np.concatenate(all_features, axis=0)
         self.logger.timed_print(
-            f"ECFP fingerprints: {ecfp_arr.shape[1]} features for {ecfp_arr.shape[0]} molecules."
+            f"Generated fingerprint array of length {fp_arr.shape[1]} for {fp_arr.shape[0]} molecules."
         )
-        return ecfp_arr
-
-    def generate_maccs(self, smiles):
-        molecule = Chem.MolFromSmiles(smiles)
-        if molecule is None:
-            return np.zeros(167, dtype=np.uint8)
-        return np.array(MACCSkeys.GenMACCSKeys(molecule), dtype=np.uint8)
-
-    def generate_maccs_arr(self, df: pl.DataFrame, smiles_col: str):
-        self.logger.timed_print(f"Generating MACCS features...")
-        smiles_arr = df[smiles_col].to_numpy()
-        maccs_arr = np.vectorize(self.generate_maccs, signature="()->(n)")
-        maccs_arr = maccs_arr(smiles_arr)
-        self.logger.timed_print(
-            f"MACCS keys: {maccs_arr.shape[1]} features for {maccs_arr.shape[0]} molecules."
-        )
-        return maccs_arr
-
-    def generate_avalon(self, smiles):
-        molecule = Chem.MolFromSmiles(smiles)
-        if molecule is None:
-            return np.zeros(512, dtype=np.float32)
-        return np.array(GetAvalonFP(molecule, nBits=512))
-
-    def generate_avalon_arr(self, df: pl.DataFrame, smiles_col: str):
-        self.logger.timed_print(f"Generating Avalon features...")
-        smiles_arr = df[smiles_col].to_numpy()
-        aval_arr = np.vectorize(self.generate_avalon, signature="()->(n)")
-        aval_arr = aval_arr(smiles_arr)
-        self.logger.timed_print(
-            f"Avalon fingerprint: {aval_arr.shape[1]} features for {aval_arr.shape[0]} molecules."
-        )
-        return aval_arr
+        return fp_arr
